25c20b98b34a2c4388c1aa97725e8f02d19c44e2
Patilsahil777 <Patilsahil2403@gmail.com>
2025-02-15 09:46:41 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+const accountId = 14443
+let accountEmail = "sahilpatil2403@gmail.com"
+var accountPassword = "12345"
+accountCity = "jailpur "
+
+
+//accountId = 2 // not allowed //
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = 212121
+accountCity = "jaipur"
+let accountState; 
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
+/* Prefer not to used var because of issue in block scope and function scope 
+*/ 
+
+
